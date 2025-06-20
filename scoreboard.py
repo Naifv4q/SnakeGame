d7a31b74c5dbe617e144275efd91c0725ac23e1d
Naif <noobprogrammer150@gmail.com>
2025-06-20 14:44:52 +0300

    Second commit: I added a new feature, a high score that the program keeps track off in a file.
    Now the game resets everytime the player loses, instead of the old code which stops the game and shows a gameover message.
    
    Lastly deleted some comments that seemed obvious and just made the code look a bit horrible to read, and modified some comments.

--- scoreboard.py
+++ scoreboard.py
@@ -4,35 +4,69 @@ class ScoreBoard(Turtle):
     def __init__(self):
         super().__init__()
         #Inherting the turtle class, to make a 'turtle' tile for the score, then setting its attributes as seen below:
+
         #Here we hide the turtle that comes from initiating the turtle object.
         self.ht()
+
+        #Adding a high score to the game, to keep track of the highest score the user ever got.
+        self.high_score= self.read_high_score()
+
         #Here we raise the pen so the object doesn't draw in the screen.
         self.penup()
+
         #Making the score font white.
         self.color("White")
-        #Setting the score location in the up right corner of the screen.
-        self.goto(-255,275)
-        #initaiting a score variable.
+
+        #Setting the score location in the upper center of the screen.
+        self.goto(0,275)
+
         self.score=0
         #Writing the score on the screen using the write() function.
-        self.write(f"Score: {self.score}", False,"Center",("Arial",15,"normal"))
-    #Simply adds +1 to the score counter when called, and clearing the previous score and updating it with the new one.
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", True,"Center",("Arial",15,"normal"))
+
+    #This method simply updates the score on the screen.
+    def update_score(self):
+        self.clear()
+        self.write(f"Score: {self.score}  High Score: {self.high_score}", True,"Center",("Arial",15,"normal"))
+        self.goto(0,275)
+
+    #A score reset, when the player loses the previous game.
+    def score_reset(self):
+        if self.score > self.high_score:
+            self.write_high_score(self.score)
+            self.high_score = self.read_high_score()
+        self.score=0
+        self.update_score()
+    
+    #A function that adds a new score on call.
     def add_score(self):
         self.score+=1
         self.clear()
-        self.write(f"Score: {self.score}", True,"Center",("Arial",15,"normal"))
-        self.goto(-255,275)
-    #A reset function for a replay.
-    def score_reset(self):
-        self.score=0
+        self.update_score()
+
+    #A function that reads the highest score in the score_memory.txt file.
+    def read_high_score(self):
+        with open("score_memory.txt",mode="r") as memory_file:
+            return int(memory_file.read())
+        
+
+    #A function that writes the highest score the user gets in the score_memory.txt file.
+    def write_high_score(self,score):
+        with open("score_memory.txt",mode="w") as memory_file:
+            memory_file.write(f"{score}")
+
+
+    #Below is an old code, I kept it for documentation.
+
     #A game over message shows when called.
-    def show_game_over(self):
-        self.game_over = Turtle()
-        self.game_over.ht()
-        self.game_over.penup()
-        self.game_over.goto(0,0)
-        self.game_over.color("red")
-        self.game_over.write("GAME OVER !",False,"Center",("Arial",15,"normal"))
+    #
+    # def show_game_over(self):
+    #     self.game_over = Turtle()
+    #     self.game_over.ht()
+    #     self.game_over.penup()
+    #     self.game_over.goto(0,0)
+    #     self.game_over.color("red")
+    #     self.game_over.write("GAME OVER !",False,"Center",("Arial",15,"normal"))
         
     
 
--- snake.py
+++ snake.py
@@ -7,12 +7,10 @@ WEST=180
 SOUTH=270
 
 class Snake:
+    #The constructor makes the start snake:
     def __init__(self):
-        #Initiating snake part list.
         self.snake_parts=[]
-        #Calling a create_snake() function to make a snake
         self.create_snake()
-        #Assigning the head to a single variable, instead of calling the whole list everytime.
         self.head=self.snake_parts[0]
 
     #A function that makes a snake on call, by making a default snake with 3 body parts.
@@ -21,12 +19,12 @@ class Snake:
         snake_body = Turtle()
         snake_head = Turtle()
 
-        #Initaiting every body part of the snake and assigning them to a snake_parts list.
+        #Making a snake parts list, to easily keep track and modify the whole snake.
         self.snake_parts = [snake_head, snake_body, snake_tail]
 
-        #go_left variable is set to decrement every part by 20 pixels to the left.
+        #go_left variable is set to decrement every part by 20 pixels to the left, so the snake doesnt overlap its other parts.
         go_left = 0
-        #Snake values are set in one loop.
+        #Initiating the snake parts:
         for snake in self.snake_parts:
             snake.shape("square")
             snake.color("green")
@@ -34,16 +32,15 @@ class Snake:
             snake.goto(go_left, 0)
             go_left -= 20
 
-    #A function that keeps the whole snake moving 20 pixels in a loop on call.
+    #A function that keeps the whole snake moving 20 pixels in a loop on call:
     def move(self):
             # The below loop, updates each part location by the part ahead, so if the head part moves 10 y-axis, the rest of the parts move that way.
             for part in range(len(self.snake_parts) - 1, 0, -1):
                 new_x = self.snake_parts[part - 1].xcor()
                 new_y = self.snake_parts[part - 1].ycor()
                 self.snake_parts[part].goto(new_x, new_y)
-            #Making the snake move by the constant distance, which is 20 pixels
             self.head.forward(MOVE_DISTANCE)
-            #If the snake body enters one of the borders, this if block makes it render in the opposite border, like the classic snake game
+            #If the snake body enters one of the borders, this if block makes it render in the opposite border, like the classic snake game.
             if self.head.xcor() > 280 :
                 self.head.goto(-280,self.head.ycor())
             elif self.head.xcor() < -295:
@@ -70,7 +67,7 @@ class Snake:
         if self.head.heading() != EAST:
             self.head.setheading(WEST)
 
-    #This function adds a tail object to the snake on call.
+    #This function adds a tail part (object) to the snake on call.
     def add_tail(self):
         new_tail=Turtle()
         new_tail.shape("square")
@@ -78,6 +75,14 @@ class Snake:
         new_tail.penup()
         new_tail.goto(self.snake_parts[-1].position())
         self.snake_parts.append(new_tail)
+
+    #New function that basically resets the snake, and get the old snake outside of the screen view.
+    def reset_snake(self):
+        for part in self.snake_parts:
+            part.goto(1000,1000)
+        self.snake_parts.clear()
+        self.create_snake()
+        self.head=self.snake_parts[0]
 
 
 
